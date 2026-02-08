232a48160a0c939a32ff0ec7a488a3bbb3d27047
Caront <carontsubs@gmail.com>
2026-02-08 20:59:14 +0100

    Canviar execuccio a la branca de treball

--- engegar.bat
+++ engegar.bat
@@ -1,6 +1,13 @@
 @echo off
 cd /d "C:\Users\User\Documents\Python\Git\Cau"
 
+:: Canvia a la branca correcta
+git checkout "feina/bot-webhooks"
+
+:: Mostra la branca actual
+git branch
+
+
 :: Obre el ngrok amb el teu domini fix
 start ngrok http --url=lakeesha-semiherbaceous-nonstudiously.ngrok-free.dev 8080
 
