5a4fd0d9afc49a7f13ed9204eac83135cb11158c
carontsubs <carontsubs@gmail.com>
2026-04-06 12:21:53 +0200

    Primer enviament

---
+++ prova.py
@@ -0,0 +1,8 @@
+# El meu primer script compatible
+import math
+
+radi = 15
+area = math.pi * pow(radi, 2)
+
+print(f"L'àrea d'un cercle amb radi {radi} és: {area:.2f}")
+print("Script executat amb èxit al Pentium D!")
